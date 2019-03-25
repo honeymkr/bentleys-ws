d4d01464c119bded19af92697d6be20eda56cc87
Jasper Fawkes <design@honeymkr.com>
2019-03-25 19:08:11 -0400

    fixing contentful setting error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,5 @@
-let contentfulConfig
+require("dotenv").config()
 
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require("./.contentful")
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
-
-const {
-  spaceId,
-  accessToken
-} = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    "Contentful spaceId and the delivery token need to be provided."
-  )
-}
 
 module.exports = {
   siteMetadata: {
@@ -45,10 +24,17 @@ module.exports = {
         }, ],
       },
     },
+    
     {
-      resolve: "gatsby-source-contentful",
-      options: contentfulConfig,
+
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+      },
     },
+
+    
     {
       resolve: `gatsby-plugin-favicon`,
       options: {
