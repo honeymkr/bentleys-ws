0592dfcbc9e7c46d010b3891f9078ca0df030fe3
HONEYMKR <HONEYMKR.NYC@GMAIL.COM>
2021-04-26 13:14:30 -0400

    edit content, change layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,16 @@
 import React, { Component } from "react"
-import { Link } from "gatsby"
 import styled from 'styled-components'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faFacebookF,
-} from "@fortawesome/free-brands-svg-icons"
-
-
-const Button = styled.button`
-  background: palevioletred;
-  border-radius: 3px;
-  border: none;
-  padding: 1rem;
-  margin-left: 0.5rem;
-  color: white;
-  font-size:20px;
-`;
-
-const TomatoButton = styled(Button)`
-  background: tomato;
-`;
 
 const Hours = styled.p`
 font-size: 17px;
 `;
 
 const Header = styled.h3`
+color:white;
+`;
+
+const Adress = styled.b`
 color:white;
 `;
 
@@ -44,37 +28,13 @@ export class Footer extends Component {
     return (
    
     <Wrapper>
-
-
-
             <Header>Hours of Operation:</Header>
-                <Hours>Monday through Friday 6:30am to 3:00pm </Hours>
-                <Hours>Closed Tuesday </Hours>
-                <Hours>Saturday and Sunday 7:30am to 1:30pm </Hours>
+                <Hours>Tuesday through Friday 6:30am to 1:30pm </Hours>
+                <Hours>Closed Sunday and Monday </Hours>
+                <Hours>Saturday 8am to 1:30pm </Hours>
 
-               <h3>Address: 20 Hill street, Danville VT 05828
-Mailing address: PO Box 77, Danville VT 05828</h3>
+               <Adress>Address: 20 Hill street, Danville VT 05828</Adress>
 
-
-                <TomatoButton > 
-                <a href="mailTo:bentleysbakeryandcafevt@gmail.com" target="_blank"
-                target="_blank" 
-                rel="noopener noreferrer"
-                > 
-                Email Us!
-                    </a>
-                </TomatoButton>
-
-                <TomatoButton >
-                <a href="https://www.facebook.com/Bentleys-Bakery-and-Cafe-159258157467790/" 
-                      target="_blank" 
-                      rel="noopener noreferrer"
-                > 
-                      <span className="icon">
-                        <FontAwesomeIcon icon={faFacebookF} />
-                      </span>
-                    </a>
-                </TomatoButton>
           </Wrapper>
     )
   }
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react"
 import Link from "gatsby-link"
 import logoImg from "../images/logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebookF,
+} from "@fortawesome/free-brands-svg-icons"
+
+
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  padding: 1rem;
+  margin-left: 0.5rem;
+  color: white;
+  font-size:20px;
+`;
+
+const TomatoButton = styled(Button)`
+  background: tomato;
+`;
 
 
 export class Header extends Component {
@@ -12,6 +31,17 @@ export class Header extends Component {
               <Link to="/" className="brand-logo">
                 <img src={logoImg} alt="brand logo" />
               </Link>
+
+              <TomatoButton >
+                <a href="https://www.facebook.com/Bentleys-Bakery-and-Cafe-159258157467790/" 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                > 
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faFacebookF} />
+                      </span>
+                    </a>
+                </TomatoButton>
           
         </div>
       </section>
